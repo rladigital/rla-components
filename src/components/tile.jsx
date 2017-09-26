8602a955a8056ead62cf97b32af712bc70f74631
Tom Lawton <tom.lawton@rla.co.uk>
2017-09-26 14:42:45 +0100

    Removing a log

--- src/components/tile.jsx
+++ src/components/tile.jsx
@@ -25,7 +25,7 @@ class Tile extends React.Component {
         const tilesize = this.state.tilesize;
         this.setState({ height: height });
 
-        console.log(tilesize);
+        //console.log(tilesize);
 
         // Scaling
         if (this.props.responsiveScale) {
