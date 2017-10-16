31e4ce8022df35e2da7247f92f6b3f76bcf16b4e
Tom Lawton <tom.lawton@rla.co.uk>
2017-10-17 00:11:51 +0100

    Updating build to allow css

--- webpack.config.js
+++ webpack.config.js
@@ -33,6 +33,10 @@ module.exports = {
                 test: /\.jsx?$/,
                 use: ["babel-loader"],
                 exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
             }
         ]
     },
