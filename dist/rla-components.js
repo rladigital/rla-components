a2c961cd385ce795abd6008aa2ce8c1b08e47ac9
RLA Digital <digital@rla.co.uk>
2017-11-21 09:12:45 +0000

    Remove ul from menu

--- dist/rla-components.js
+++ dist/rla-components.js
@@ -47888,8 +47888,7 @@ Object.defineProperty(exports, "__esModule", {
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-var _templateObject = _taggedTemplateLiteral(["\n    color: ", ";\n    background: ", ";\n    margin: 0 0 ", "em 0;\n"], ["\n    color: ", ";\n    background: ", ";\n    margin: 0 0 ", "em 0;\n"]),
-    _templateObject2 = _taggedTemplateLiteral(["\n    list-style: none;\n    padding: 0;\n    margin: 0;\n"], ["\n    list-style: none;\n    padding: 0;\n    margin: 0;\n"]);
+var _templateObject = _taggedTemplateLiteral(["\n    color: ", ";\n    background: ", ";\n    margin: 0 0 ", "em 0;\n"], ["\n    color: ", ";\n    background: ", ";\n    margin: 0 0 ", "em 0;\n"]);
 
 var _react = __webpack_require__(1);
 
@@ -47916,7 +47915,6 @@ var Container = _styledComponents2.default.div(_templateObject, function (props)
 }, function (props) {
     return props.theme.navigation.margin;
 });
-var Menu = _styledComponents2.default.ul(_templateObject2);
 
 var MainNavMenu = function (_React$Component) {
     _inherits(MainNavMenu, _React$Component);
@@ -47933,11 +47931,7 @@ var MainNavMenu = function (_React$Component) {
             return _react2.default.createElement(
                 Container,
                 this.props,
-                _react2.default.createElement(
-                    Menu,
-                    null,
-                    this.props.children
-                )
+                this.props.children
             );
         }
     }]);
