317bb68895f77aa848175cfbcb95e602e11e3c8d
Tom Lawton <tom.lawton@rla.co.uk>
2017-12-08 15:23:22 +0000

    Improving prop types in the TableList

--- src/components/TableList/TableList.jsx
+++ src/components/TableList/TableList.jsx
@@ -64,9 +64,31 @@ class TableList extends React.Component {
 TableList.displayName = "TableList";
 
 TableList.propTypes = {
-    children: PropTypes.array, //TODO - Work on ensuring table > [thead > tr > th, tbody > *tr > td] layout
+    /** Controls the type of element rendered, one of "responsive", "list", "table" */
     type: PropTypes.oneOf(["responsive", "list", "table"]),
-    respondsAt: PropTypes.number
+
+    /** The pixel value to swap to a list from a table */
+    respondsAt: PropTypes.number,
+
+    /** The TableList requires a single thead and tbody as direct chilren */
+    children: function(props, propName, componentName) {
+        const tableElements = React.Children.toArray(props["children"]);
+        if (!Array.isArray(tableElements) || tableElements.length !== 2) {
+            return new Error(
+                "The TableList must contain a single thead and single tbody as direct children."
+            );
+        }
+        if (tableElements[0].type !== "thead") {
+            return new Error(
+                "The TableList must contain a thead as the first element"
+            );
+        }
+        if (tableElements[1].type !== "tbody") {
+            return new Error(
+                "The TableList must contain a tbody as the first element"
+            );
+        }
+    }
 };
 
 TableList.defaultProps = {
