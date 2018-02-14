b6a98374d9fe3a7816a1507bca09a4bcc1ee7ce5
RichSelby <richselby@gmail.com>
2018-02-14 11:34:08 +0000

    Altering Checkbox buttons to improve storybook presentation

--- src/components/form/checkboxButtons.js
+++ src/components/form/checkboxButtons.js
@@ -1,13 +1,13 @@
 import React from "react";
-import Button from "../button"
+import Button from "../button";
 import FormLabel from "./label";
 import PropTypes from "prop-types";
 import isArray from "lodash/isArray";
 import InputError from "./inputError";
-import FontAwesomeIcon from '@fortawesome/react-fontawesome'
-import faSquare from '@fortawesome/fontawesome-free-solid/faSquare'
-import faCheckSquare from '@fortawesome/fontawesome-free-solid/faCheckSquare'
-import {withTheme} from "styled-components";
+import FontAwesomeIcon from "@fortawesome/react-fontawesome";
+import faSquare from "@fortawesome/fontawesome-free-solid/faSquare";
+import faCheckSquare from "@fortawesome/fontawesome-free-solid/faCheckSquare";
+import { withTheme } from "styled-components";
 
 class CheckboxButtons extends React.Component {
     constructor(props) {
@@ -19,10 +19,7 @@ class CheckboxButtons extends React.Component {
     }
 
     componentWillReceiveProps(nextProps) {
-        if (
-            nextProps.value !== this.props.value &&
-            isArray(nextProps.value)
-        ) {
+        if (nextProps.value !== this.props.value && isArray(nextProps.value)) {
             this.setState({
                 values: nextProps.value
             });
@@ -31,37 +28,49 @@ class CheckboxButtons extends React.Component {
 
     handleChange = value => {
         const { onChange } = this.props;
-        let values = [ ...this.state.values ];
+        let values = [...this.state.values];
         let valuePosition = values.indexOf(value);
 
         if (valuePosition != -1) {
             values.splice(valuePosition, 1);
-        }
-        else {
+        } else {
             values.push(value);
         }
 
-        this.setState({ values }, () => onChange({ name: this.props.name, value: values }));
-    }
+        this.setState({ values }, () =>
+            onChange({ name: this.props.name, value: values })
+        );
+    };
 
     render() {
         const { values } = this.state;
-        const { theme, options, error, buttonProps: { defaultColor, activeColor, ...buttonProps }, ...rest } = this.props;
-        
+        const {
+            theme,
+            options,
+            error,
+            buttonProps: { defaultColor, activeColor, ...buttonProps },
+            ...rest
+        } = this.props;
+
         const buttons = options.map(option => {
             let isChecked = values.indexOf(option.value) > -1;
-            let params    = {
+            let params = {
                 ...buttonProps,
-                key    : option.value,
-                color  : isChecked ? (activeColor || theme.checkboxButton.activeColor) : (defaultColor || theme.checkboxButton.defaultColor),
+                key: option.value,
+                color: isChecked
+                    ? activeColor || theme.checkboxButton.activeColor
+                    : defaultColor || theme.checkboxButton.defaultColor,
                 onClick: () => this.handleChange(option.value)
             };
 
             return [
                 <Button {...params}>
-                    <FontAwesomeIcon icon={isChecked ? faCheckSquare : faSquare} />
-                    {' '}{option.text}
-                </Button>," "
+                    <FontAwesomeIcon
+                        icon={isChecked ? faCheckSquare : faSquare}
+                    />{" "}
+                    {option.text}
+                </Button>,
+                " "
             ];
         });
 
@@ -71,15 +80,16 @@ class CheckboxButtons extends React.Component {
                     <FormLabel {...rest}>{this.props.label}</FormLabel>
                 )}
 
-                <div>
-                    {buttons}
-                </div>
+                <div>{buttons}</div>
 
                 <InputError error={error} />
             </div>
         );
     }
 }
+
+CheckboxButtons = withTheme(CheckboxButtons);
+
 CheckboxButtons.displayName = "CheckboxButtons";
 
 CheckboxButtons.propTypes = {
@@ -98,8 +108,8 @@ CheckboxButtons.propTypes = {
 };
 
 CheckboxButtons.defaultProps = {
-    error: '',
+    error: "",
     buttonProps: {}
 };
 
-export default withTheme(CheckboxButtons);
+export default CheckboxButtons;
