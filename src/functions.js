758fb52a1d78ec201e6e9bf501055620abae1027
RLA Digital <digital@rla.co.uk>
2018-05-21 16:43:08 +0100

    Remove console.log

--- src/functions.js
+++ src/functions.js
@@ -116,7 +116,6 @@ export function luminanace(hex) {
         return v <= 0.03928 ? v / 12.92 : Math.pow((v + 0.055) / 1.055, 2.4);
     });
 
-    console.log(rgb, [0] * 0.2126 + a[1] * 0.7152 + a[2] * 0.0722);
     return a[0] * 0.2126 + a[1] * 0.7152 + a[2] * 0.0722;
 }
 
