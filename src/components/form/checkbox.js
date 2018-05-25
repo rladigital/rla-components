76326f9ec83d8fab79394b2b8bfec8aa903d0463
RLA Digital <digital@rla.co.uk>
2018-05-25 18:15:57 +0100

    Working on custom checkboxes

--- src/components/form/checkbox.js
+++ src/components/form/checkbox.js
@@ -3,9 +3,61 @@ import PropTypes from "prop-types";
 import isUndefined from "lodash/isUndefined";
 import isArray from "lodash/isArray";
 import styled, { css } from "styled-components";
+
 import { shade } from "../../functions";
 import FormLabel from "./label";
 import InputError from "./inputError";
+
+const G = styled.g`
+    transform: translate(50%, 50%);
+`;
+
+const CheckboxCheck = styled.path.attrs({
+    d:
+        "M-2.73,6.11-8.29.56a.86.86,0,0,1,0-1.21l1.21-1.21a.86.86,0,0,1,1.21,0l3.75,3.75,8-8a.86.86,0,0,1,1.21,0L8.29-4.9a.86.86,0,0,1,0,1.21L-1.52,6.11A.86.86,0,0,1-2.73,6.11Z"
+})`
+    transform: scale(0);
+    fill: ${props => props.theme.colors.primary};
+    transition: transform 0.25s ease;
+`;
+
+const CheckboxSquare = styled.rect.attrs({
+    width: 20,
+    height: 20,
+    rx: 3,
+    ry: 3
+})`
+    stroke-width: 1px;
+    fill: ${props => props.theme.colors.black};
+    stroke: ${props => props.theme.colors.mediumGray};
+`;
+
+const Checkbox = styled.svg.attrs({
+    width: 20,
+    height: 20,
+    viewBox: "0 0 20 20"
+})`
+    display: inline-block;
+    vertical-align: middle;
+`;
+
+const Input = styled.input`
+    display: none;
+    &:checked + ${Checkbox} ${CheckboxCheck} {
+        transform: scale(1);
+    }
+`;
+
+const Wrapper = styled.label`
+    margin-bottom: ${props => props.theme.spacing.margin}rem;
+    display: ${props => (props.inlineRadioButtons ? "inline-block" : "block")};
+    padding-right: ${props => props.theme.spacing.margin}rem;
+    cursor: pointer;
+`;
+
+const Text = styled.span`
+    vertical-align: middle;
+`;
 
 class MultiCheckbox extends Component {
     constructor(props) {
@@ -51,19 +103,23 @@ class MultiCheckbox extends Component {
             const isChecked = values.indexOf(option.value) > -1;
 
             return (
-                <div key={option.value}>
-                    <label>
-                        <input
-                            type="checkbox"
-                            onChange={event =>
-                                this.handleChange(event, option.value)
-                            }
-                            checked={isChecked}
-                            value={option.value}
-                        />
-                        {option.text}
-                    </label>
-                </div>
+                <Wrapper key={option.value}>
+                    <Input
+                        type="checkbox"
+                        onChange={event =>
+                            this.handleChange(event, option.value)
+                        }
+                        checked={isChecked}
+                        value={option.value}
+                    />
+                    <Checkbox>
+                        <CheckboxSquare />
+                        <G>
+                            <CheckboxCheck />
+                        </G>
+                    </Checkbox>
+                    <Text>{" " + option.text}</Text>
+                </Wrapper>
             );
         });
 
