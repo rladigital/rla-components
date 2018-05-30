0592e65dbbb73c561c232202771525cf7c499e2d
RLA Digital <digital@rla.co.uk>
2018-05-30 15:58:46 +0100

    Tweaks to hero and footer

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,17 +1,19 @@
 import PropTypes from "prop-types";
 import styled from "styled-components";
-import { StyledPanel } from "./panel";
 import { colors, spacing, sizes } from "../theme";
 
 const panel = props => props.theme.panel.types[props.type];
 
-const Hero = StyledPanel.extend`
+const Hero = styled.div`
+    width: 100%;
     height: ${props => props.height}px;
     background-image: url('${props => props.backgroundImage}');
     background-size: cover;
     background-position: center;
     background-size: ${props => props.backgroundSize};
     background-repeat: no-repeat;
+    margin-bottom: ${props => props.theme.spacing.margin}em;
+    padding-top: ${props => props.theme.spacing.padding}em;
 `;
 
 Hero.displayName = "Hero";
